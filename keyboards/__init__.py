9cefcd457b7d7337c36e65fab62d621ca3fa6c8b
developerhizol <kostatrofimcev11@gmail.com>
2026-06-19 13:37:09 +0300

    Add files via upload

---
+++ keyboards/__init__.py
@@ -0,0 +1,11 @@
+from .main_menu import get_main_keyboard
+from .about_service_keyboard import get_about_service_keyboard
+from .help_keyboard import get_help_keyboard
+from .admin_keyboards import (
+    get_admin_main_keyboard,
+    get_admin_back_keyboard,
+    get_cancel_keyboard,
+    get_confirm_keyboard,
+    get_servers_management_keyboard,
+    get_broadcast_choice_keyboard
+)
---
+++ keyboards/about_service_keyboard.py
@@ -0,0 +1,28 @@
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+
+def get_about_service_keyboard():
+    builder = InlineKeyboardBuilder()
+    
+    builder.button(
+        text="Тех. поддержка",
+        url="https://t.me/StreamNetAdmin",
+        icon_custom_emoji_id="5258093637450866522"
+    )
+    
+    builder.button(
+        text="Пользовательское соглашение",
+        url="https://telegra.ph/Polzovatelskoe-soglashenie-04-01-19",
+        icon_custom_emoji_id="5257965810634202885"
+    )
+    
+    builder.button(
+        text="Политика конфиденциальности",
+        url="https://telegra.ph/Politika-konfidencialnosti-04-01-26",
+        icon_custom_emoji_id="5258476306152038031"
+    )
+ 
+    builder.button(text="« Назад", callback_data="back_to_menu")
+    
+    builder.adjust(1)
+    return builder.as_markup()
