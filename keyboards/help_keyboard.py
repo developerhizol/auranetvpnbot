27a7c1e25f9a0e19dddda25d4eda9d9e0b02a5ec
developerhizol <kostatrofimcev11@gmail.com>
2026-08-30 13:51:38 +0300

    Delete keyboards directory

--- keyboards/help_keyboard.py
+++
@@ -1,7 +0,0 @@
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from aiogram.utils.keyboard import InlineKeyboardBuilder
-
-def get_help_keyboard():
-    builder = InlineKeyboardBuilder()
-    builder.button(text="« Назад", callback_data="back_to_menu")
-    return builder.as_markup()
