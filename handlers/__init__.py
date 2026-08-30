88dabcf350e547fb8ef628e236ac012bee895f4c
developerhizol <kostatrofimcev11@gmail.com>
2026-08-30 13:51:31 +0300

    Delete handlers directory

--- handlers/__init__.py
+++
@@ -1,8 +0,0 @@
-from .start import router as start_router
-from .about_service import router as about_service_router
-from .help import router as help_router
-from .admin import router as admin_router
-from .payment import router as payment_router
-from .profile import router as profile_router
-
-__all__ = ["start_router", "about_service_router", "help_router", "admin_router", "payment_router", "profile_router"]
